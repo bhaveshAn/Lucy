4e0f0e5774d8fc4e383116fa8a7b1821b3715181
bhaveshAn <bhaveshanand7@gmail.com>
2017-12-20 21:01:31 +0530

    Add runtime.txt

--- main.py
+++ main.py
@@ -33,4 +33,5 @@ def ask():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get('PORT', 7001))
+    app.run(host='0.0.0.0', port=port, debug=True)
